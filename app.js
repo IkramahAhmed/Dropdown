3940ad783b00c8ba5a6dbe0d2ce891fda7670339
Ikramah <92651495+IkramahAhmed@users.noreply.github.com>
2022-09-09 05:43:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// function btnclick(){
+//     alert("button was clicked");
+// }
+
+
+// var input=document.getElementById("inp");
+// var btn = document.getElementById("btn");
+// var para=document.getElementById("para1");
+
+// function search(){
+//     flag=false;
+//     var city = ["karachi","lahore","multan"];
+// for(i=0;i<city.length;i++){
+//     if(input.value==city[i]){
+//          para.innerHTML="correct";
+//     // console.log("found");
+//     flag=true;
+//     }
+//     // else{
+//     //     console.log("not found");
+//     // }
+// }
+// if(flag !=true){
+//     // console.log("not found");
+// para.innerHTML="not correct";
+// }
+    
+// }
+//if lenght is above 3
+// var input=document.getElementById("inp");
+// var input2=document.getElementById("bt2");
+
+// var btn = document.getElementById("btn");
+
+// function search(){
+//     if(input.value.length<3){
+// input2.value="please enter lenght above 3";
+//     }
+//     else{
+//         alert("correct");
+
+//     }
+// }
+
+  
